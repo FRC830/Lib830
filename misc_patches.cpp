150a8c488effbdcca3d86c6511cd322b2b72e203
cbott <cbott6@gmail.com>
2016-03-11 20:37:47 -0500

    get rid of definition in misc_patches.cpp

--- misc_patches.cpp
+++ misc_patches.cpp
@@ -1,5 +1,3 @@
 #include <set>
 #include "WPILib.h"
-// work around https://usfirst.collab.net/sf/go/artf4798
-// if this fails, comment out this line
-std::set<Ultrasonic*> Ultrasonic::m_sensors;
+// nothing to see here
