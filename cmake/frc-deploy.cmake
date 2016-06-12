7ab3163342a449f9ff7167fadcd8bcfa54438cee
lethosor <lethosor@gmail.com>
2016-06-12 13:44:55 -0400

    Fix deploy script

--- cmake/frc-deploy.cmake
+++ cmake/frc-deploy.cmake
@@ -20,11 +20,35 @@ math(EXPR TEAM_NUMBER_LOW "${TEAM_NUMBER} % 100")
 
 set(USERNAME lvuser)
 set(DEPLOY_DIR /home/lvuser)
+set(TMP_SSH_HOSTS tmp_ssh_hosts.txt)
+set(SSH_FLAGS -o StrictHostKeyChecking=no -o UserKnownHostsFile=${TMP_SSH_HOSTS})
+file(WRITE ${TMP_SSH_HOSTS} "")
+
+function(log MESSAGE)
+    message("==> ${MESSAGE}")
+endfunction()
+
+function(run_ssh MESSAGE EXE)
+    log("${MESSAGE}")
+    list(REMOVE_AT ARGV 0 1)
+    string(TOLOWER ${EXE} EXE)
+    if(${EXE} STREQUAL ssh)
+        set(EXE ${SSH_EXECUTABLE})
+    elseif(${EXE} STREQUAL scp)
+        set(EXE ${SCP_EXECUTABLE})
+    else()
+        message(FATAL_ERROR "invalid executable: ${EXE} (needs ssh/scp)")
+    endif()
+    execute_process(COMMAND ${EXE} ${SSH_FLAGS} ${ARGV} RESULT_VARIABLE STATUS)
+    if (NOT(${STATUS} EQUAL 0))
+        message(FATAL_ERROR "SSH failed")
+    endif()
+endfunction()
 
 set(TARGET)
 foreach(CUR_TARGET "roboRIO-${TEAM_NUMBER}-FRC.local" "172.22.11.2" "10.${TEAM_NUMBER_HIGH}.${TEAM_NUMBER_LOW}.2")
-    message("Trying ${CUR_TARGET}")
-    execute_process(COMMAND ${SSH_EXECUTABLE} ${USERNAME}@${CUR_TARGET} rm -f ${DEPLOY_DIR}/FRCUserProgram
+    log("Trying ${CUR_TARGET}")
+    execute_process(COMMAND ${SSH_EXECUTABLE} ${SSH_FLAGS} ${USERNAME}@${CUR_TARGET} true
         RESULT_VARIABLE SSH_RESULT
         TIMEOUT 5
     )
@@ -36,38 +60,30 @@ endforeach()
 if (NOT TARGET)
     message(FATAL_ERROR "Could not find target")
 endif()
-message("Connected to ${TARGET}")
+log("Connected to ${TARGET}")
 
-function(run_ssh_safe MESSAGE)
-    message("${MESSAGE}")
-    list(REMOVE_AT ARGV 0)
-    execute_process(COMMAND ${ARGV} RESULT_VARIABLE STATUS)
-    if (NOT(${STATUS} EQUAL 0))
-        message(FATAL_ERROR "SSH failed")
-    endif()
-endfunction()
-
-run_ssh_safe("Copying over robot code"
-    ${SCP_EXECUTABLE}
-        ${ROBOT_EXECUTABLE}
-        ${USERNAME}@${TARGET}:${DEPLOY_DIR}/FRCUserProgram
+run_ssh("Copying over robot code"
+    scp
+    ${ROBOT_EXECUTABLE}
+    ${USERNAME}@${TARGET}:${DEPLOY_DIR}/FRCUserProgram
 )
-run_ssh_safe("Stopping netconsole-host"
-    ${SSH_EXECUTABLE}
-        ${USERNAME}@${TARGET}
-        killall -q netconsole-host || :
+run_ssh("Stopping netconsole-host"
+    ssh
+    ${USERNAME}@${TARGET}
+    "killall -q netconsole-host || :"
 )
-run_ssh_safe("Copying over robot start script"
-    ${SCP_EXECUTABLE}
-        ${WPILIB_HOME}/ant/robotCommand
-        ${USERNAME}@${TARGET}:${DEPLOY_DIR}
+run_ssh("Copying over robot start script"
+    scp
+    ${WPILIB_HOME}/ant/robotCommand
+    ${USERNAME}@${TARGET}:${DEPLOY_DIR}
 )
-run_ssh_safe("Restarting robot"
-    ${SSH_EXECUTABLE}
-        ${USERNAME}@${TARGET}
-        . /etc/profile.d/natinst-path.sh;
-        chmod a+x ${DEPLOY_DIR}/FRCUserProgram;
-        /usr/local/frc/bin/frcKillRobot.sh -t -r;
-        sync
+run_ssh("Restarting robot"
+    ssh
+    ${USERNAME}@${TARGET}
+    ". /etc/profile.d/natinst-path.sh\\\\;"
+    "chmod a+x ${DEPLOY_DIR}/FRCUserProgram\\\\;"
+    "/usr/local/frc/bin/frcKillRobot.sh -t -r\\\\;"
+    sync
 )
-message("Done.")
+log("Done.")
+file(REMOVE ${TMP_SSH_HOSTS})
